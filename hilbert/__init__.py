feb9b248036779b2c826582129244073f0b776ec
Camp <chc@NIST.GOV>
2020-06-24 15:24:00 -0400

    Updating for when ML-Hilbert is released

--- hilbert/__init__.py
+++ hilbert/__init__.py
@@ -1,5 +1,8 @@
 from .hilbert import hilbertfft
+from .mlhilbert import MLHilb
+from .synthdata import GaussianTrainingData, LorentzianTrainingData, SincTrainingData, VoigtTrainingData
 
 from ._version import __version__
 
-__all__ = ['hilbertfft', '__version__']
+__all__ = ['hilbertfft', '__version__', 'MLHilb', 'GaussianTrainingData', 'LorentzianTrainingData', 
+           'SincTrainingData', 'VoigtTrainingData']
