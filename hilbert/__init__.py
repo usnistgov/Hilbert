e74b482615c05cf3f58eb022e7a13ac7130a5098
Camp <chc@NIST.GOV>
2020-06-26 15:24:37 -0400

    Updated

--- hilbert/__init__.py
+++ hilbert/__init__.py
@@ -1,8 +1,9 @@
-from .hilbert import hilbertfft
+from .dft import hilbert_fft
 from .mlhilbert import MLHilb
 from .synthdata import GaussianTrainingData, LorentzianTrainingData, SincTrainingData, VoigtTrainingData
+from .preprocess import pad_edge_mean, mirror
 
 from ._version import __version__
 
-__all__ = ['hilbertfft', '__version__', 'MLHilb', 'GaussianTrainingData', 'LorentzianTrainingData', 
-           'SincTrainingData', 'VoigtTrainingData']
+__all__ = ['hilbert_fft', '__version__', 'MLHilb', 'GaussianTrainingData',
+           'LorentzianTrainingData', 'SincTrainingData', 'VoigtTrainingData']
