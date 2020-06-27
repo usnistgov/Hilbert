b47bb74ea2fa451c1508c1b47435455784be007d
Camp <chc@NIST.GOV>
2020-06-27 13:32:57 -0400

    hilbert_fft points to ..._henrici

--- hilbert/__init__.py
+++ hilbert/__init__.py
@@ -1,6 +1,9 @@
-from .dft import hilbert_fft
+from .dft import hilbert_fft_henrici, hilbert_fft_marple
 from .preprocess import pad_edge_mean, mirror
 
 from ._version import __version__
 
-__all__ = ['hilbert_fft', '__version__', 'pad_edge_mean', 'mirror']
+# Can be set to whatever flavor of Hilbert-fft a user wants to use
+hilbert_fft = hilbert_fft_henrici 
+
+__all__ = ['hilbert_fft', 'hilbert_fft_marple', 'hilbert_fft_henrici', '__version__', 'pad_edge_mean', 'mirror']
