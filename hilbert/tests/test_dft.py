c8dd626c0f5c52eeaedc57f7807d1e9920b50857
Camp <chc@NIST.GOV>
2020-07-14 09:55:57 -0400

    Added 2D

--- hilbert/tests/test_dft.py
+++ hilbert/tests/test_dft.py
@@ -19,6 +19,12 @@ def test_hilbert_fft_henrici():
     assert_array_almost_equal(hilb_x_analytical, hilb_x, decimal=2)
     assert np.allclose(0, hilb_x.sum()) # DHT enforced -- not real
 
+    # 2D version
+    x2 = np.vstack((x, x/2))
+    hilb_x = hilbert_fft_henrici(x2)
+    hilb_x_analytical = np.vstack((n/(2**2 + n**2), 0.5*n/(2**2 + n**2)))
+    assert_array_almost_equal(hilb_x_analytical, hilb_x, decimal=2)
+
 def test_hilbert_henrici_replace_min_value():
     """Replace min_value"""
     n = np.linspace(-100, 100, 1000)
@@ -41,6 +47,12 @@ def test_hilbert_marple():
     hilb_x_analytical = n/(2**2 + n**2)
     assert_array_almost_equal(hilb_x_analytical, hilb_x, decimal=2)
     assert np.allclose(0, hilb_x.sum()) # DHT enforced -- not real
+
+    # 2D version
+    x2 = np.vstack((x, x/2))
+    hilb_x = hilbert_fft_marple(x2)
+    hilb_x_analytical = np.vstack((n/(2**2 + n**2), 0.5*n/(2**2 + n**2)))
+    assert_array_almost_equal(hilb_x_analytical, hilb_x, decimal=2)
 
 def test_hilbert_marple_replace_min_value():
     """Replace min_value"""
