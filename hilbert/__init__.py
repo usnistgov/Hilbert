9249ae0ca4ea9646dac5ac452ff6882e28c1bf15
Camp <chc@NIST.GOV>
2020-07-14 11:48:22 -0400

    Added pad and deprecated the other padding fcns

--- hilbert/__init__.py
+++ hilbert/__init__.py
@@ -1,11 +1,12 @@
 from .dft import hilbert_fft_henrici, hilbert_fft_marple
 from .wavelet import hilbert_haar
 
-from .preprocess import pad_edge_mean, mirror
+from .preprocess import pad
 
 from ._version import __version__
 
 # Can be set to whatever flavor of Hilbert-fft a user wants to use
 hilbert_fft = hilbert_fft_henrici 
 
-__all__ = ['hilbert_fft', 'hilbert_fft_marple', 'hilbert_fft_henrici', '__version__', 'pad_edge_mean', 'mirror']
+__all__ = ['hilbert_fft', 'hilbert_fft_marple', 'hilbert_fft_henrici', 
+           '__version__', 'pad']
--- hilbert/tests/test_preprocess.py
+++ hilbert/tests/test_preprocess.py
@@ -6,14 +6,14 @@ Testing for preprocessing methods (signal manipulation prior to DHT)
 import numpy as np
 from numpy.testing import assert_array_almost_equal
 
-from hilbert.preprocess import pad_edge_mean, mirror
+from hilbert.preprocess import pad
 
 def test_pad():
     """ Test out padding procedure """
     
     #1D case
     x = np.linspace(-100, 100, 1000)
-    x_pad, window = pad_edge_mean(x, pad_width=x.shape[-1], n_edge=1, axis=-1)
+    x_pad, window = pad(x, pad_width=x.shape[-1], stat_length=1, axis=-1)
 
     assert x_pad.size == x.size*3
     assert x_pad[window].size == x.size
@@ -23,7 +23,7 @@ def test_pad():
     #2D case, axis=-1
     x = np.linspace(-100, 100, 1000)
     x = np.repeat(x[None,:], repeats=2, axis=0)
-    x_pad, window = pad_edge_mean(x, pad_width=x.shape[-1], n_edge=1, axis=-1)
+    x_pad, window = pad(x, pad_width=x.shape[-1], stat_length=1, axis=-1)
 
     assert x_pad.size == x.size*3
     assert np.allclose(True, x_pad[...,window] == x)
@@ -33,7 +33,7 @@ def test_pad():
     #2D case, axis=0
     x = np.linspace(-100, 100, 1000)
     x = np.repeat(x[:,None], repeats=2, axis=1)
-    x_pad, window = pad_edge_mean(x, pad_width=x.shape[0], n_edge=1, axis=0)
+    x_pad, window = pad(x, pad_width=x.shape[0], stat_length=1, axis=0)
 
     assert x_pad.size == x.size*3
     assert np.allclose(True, x_pad[window, ...] == x)
@@ -46,49 +46,40 @@ def test_mirror():
     
     #1D case
     x = np.linspace(-100, 100, 1000)
-    x_mirror = mirror(x, axis=-1, prepend=False)
+    x_mirror, window = pad(x, pad_width=x.size, mode='symmetric', axis=-1)
 
-    assert x_mirror.size == x.size*2
+    assert x_mirror.size == x.size*3
     assert x_mirror[-1] == x[0]
-    assert x_mirror[0] == x[0]
-    assert x_mirror[x.size-1] == x_mirror[x.size]
-    assert x_mirror[x.size-1] == x[-1]
-
-    # assert x_pad[window].size == x.size
-    # assert np.allclose(x_pad[:x.size], x[0])
-    # assert np.allclose(x_pad[-x.size:], x[-1])
+    assert x_mirror[0] == x[-1]
+    assert x_mirror[x.size-1] == x_mirror[-1]
+    assert x_mirror[x.size-1] == x[0]
 
     #2D case, axis=-1
     x = np.linspace(-100, 100, 1000)
     x = np.repeat(x[None,:], repeats=2, axis=0)
-    x_mirror = mirror(x, axis=-1, prepend=False)
+    x_mirror, window = pad(x, pad_width=x.shape[-1], mode='symmetric', axis=-1)
 
-    assert x_mirror.size == x.size*2
+    assert window.sum() == x.shape[-1]
+    assert x_mirror.shape[0] == x.shape[0]
+    assert x_mirror.shape[1] == x.shape[1]*3
+    assert x_mirror.size == x.size*3
     assert np.allclose(x_mirror[...,-1], x[0,0])
-    assert np.allclose(x_mirror[...,0], x[0,0])
+    assert np.allclose(x_mirror[...,0], x[-1,-1])
     assert np.allclose(x_mirror[...,x.shape[-1]-1], x_mirror[...,x.shape[-1]])
-    assert np.allclose(x_mirror[...,x.shape[-1]-1], x[...,-1])
+    assert np.allclose(x_mirror[...,x.shape[-1]-1], x[...,0])
 
     #2D case, axis=0
     x = np.linspace(-100, 100, 1000)
     x = np.repeat(x[:,None], repeats=2, axis=1)
-    x_mirror = mirror(x, axis=0, prepend=False)
+    x_mirror, window = pad(x, pad_width=x.shape[0], mode='symmetric', axis=0)
 
-    assert x_mirror.size == x.size*2
+    assert window.sum() == x.shape[0]
+    assert x_mirror.shape[-1] == x.shape[-1]
+    assert x_mirror.shape[0] == x.shape[0]*3
+    assert x_mirror.size == x.size*3
+    assert np.allclose(x_mirror[0,...], x[-1,-1])
     assert np.allclose(x_mirror[-1,...], x[0,0])
-    assert np.allclose(x_mirror[0,...], x[0,0])
     assert np.allclose(x_mirror[x.shape[0]-1,...], x_mirror[x.shape[0],...])
-    assert np.allclose(x_mirror[x.shape[0]-1,...], x[-1,...])
-
-    # x = np.linspace(-100, 100, 1000)
-    # x = np.repeat(x[:,None], repeats=2, axis=1)
-    # x_mirror = mirror(x, axis=-1, prepend=False)
-
-    # assert x_mirror.size == x.size*2
-    # assert np.allclose(x_mirror[...,-1], x[0,0])
-    # assert np.allclose(x_mirror[...,0], x[0,0])
-    # assert np.allclose(x_mirror[...,x.shape[-1]-1], x_mirror[...,x.shape[-1]])
-    # assert x_mirror[...,x.size-1] == x_mirror[x.size]
-    # assert x_mirror[...,x.size-1] == x[-1]
+    assert np.allclose(x_mirror[x.shape[0]-1,...], x[0,...])
     
 
