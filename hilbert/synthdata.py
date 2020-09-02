4f3541aae0920fec9c391e28c4ae8baf52ed2ee4
Camp <chc@NIST.GOV>
2020-09-02 14:42:34 -0400

    Improved docstrings

--- hilbert/synthdata.py
+++ hilbert/synthdata.py
@@ -2,18 +2,16 @@
 Module for generating synthetic data.
 """
 
-from abc import (ABC, abstractclassmethod, abstractproperty, 
-                 abstractstaticmethod)
-import itertools
+from abc import (ABC, abstractclassmethod)
 
 import numpy as np
 
-from scipy.special import dawsn, wofz, expit
+from scipy.special import dawsn
 
-class AbstractRandomTrainingDataGenerator(ABC):    
+class AbstractRandomTrainingDataGenerator(ABC):
     """ Abstract class for generating synthetic data. Assumes a lineshape has
     exactly 1 amplitude, width, and center parameter
-    
+
     Parameters
     ----------
     n : array-like, shape (n_features,)
@@ -25,35 +23,35 @@ class AbstractRandomTrainingDataGenerator(ABC):
     random_state : int, optional
         Random number generator seed, by default None
     amp : float, list, or function-like
-        Amplitude constant, list of [min,max) for uniform distribution, 
+        Amplitude constant, list of [min,max) for uniform distribution,
         or function to generate an amplitude.
     center : float, list, or function-like
-        Center-n constant, list of [min,max) for uniform distribution, 
+        Center-n constant, list of [min,max) for uniform distribution,
         or function to generate an amplitude.
     width : float, list, or function-like
-        Width constant, list of [min,max) for uniform distribution, 
+        Width constant, list of [min,max) for uniform distribution,
         or function to generate an amplitude.
-    
+
     Attributes
     ----------
     config : dict
         Configuration settings
-        
+
     """
-    
+
     # Configuration dictionary (example from Gaussian)
     m_width_is_fwhm = 2*np.sqrt(2*np.log(2))
     config = {'m_widths_from_edge' : 0.5 * m_width_is_fwhm,  # 1/2 FWHMs from edge
               'width_over_Dn_is_max_width' : 1 / m_width_is_fwhm,  # 1 FWHM is max width
               'width_over_dn_is_min_width' : 3 / m_width_is_fwhm  # min FWHM = 3 dn
              }
-    
-    def __init__(self, n, stack_Hf_f=False, n_samples=1000, random_state=None, 
-                 amp=1., center=None, width=None):   
-        
+
+    def __init__(self, n, stack_Hf_f=False, n_samples=1000, random_state=None,
+                 amp=1., center=None, width=None):
+
         # Whether to re-update after changing params
         # Before all params are initially set, we don't want this
-        self._can_update = False  
+        self._can_update = False
         self.n = n
 
         self._amp = None
@@ -62,23 +60,27 @@ class AbstractRandomTrainingDataGenerator(ABC):
         self._n_samples = None
         self._stack_Hf_f = None
 
+        self._amp_fcn = None
+        self._width_fcn = None
+        self._center_fcn = None
+
         self.amp = amp
         self.width = width
         self.center = center
         self.n_samples = n_samples
         self.stack_Hf_f = stack_Hf_f
-        
+
         np.random.seed(random_state)
-        
+
         self.f_ = None
         self.Hf_ = None
         self.conditions_ = None
-        
+
         self.generate_conditions()
         self.generate()
 
         self._can_update = True
-    
+
     def regenerate(self):
         """Create new conditions and regenerate f_ and Hf"""
         if self._can_update:
@@ -125,7 +127,7 @@ class AbstractRandomTrainingDataGenerator(ABC):
             self._width_fcn = self._ret_val_fcn([self.min_width, self.max_width])
         else:
             self._width_fcn = self._ret_val_fcn(self._width)
-        
+
         self.regenerate()
 
     @property
@@ -161,11 +163,11 @@ class AbstractRandomTrainingDataGenerator(ABC):
     @property
     def dn(self):
         return self.n[1] - self.n[0]
-    
+
     @property
     def Dn(self):
         return self.n[-1] - self.n[0]
-    
+
     @property
     def min_width(self):
         return self.config['width_over_dn_is_min_width'] * self.dn
@@ -184,16 +186,16 @@ class AbstractRandomTrainingDataGenerator(ABC):
         """Generate amplitude, width, center conditions for simulation"""
 
         # TODO: Refactor for improved memory usage
-                
+
         ctr_list = []
         amp_list = []
         width_list = []
 
         remaining_samples = self.n_samples
-        
+
         while remaining_samples > 0:
             temp_widths = np.array([self._width_fcn() for _ in range(remaining_samples)])
-            
+
             temp_amps = np.array([self._amp_fcn() for _ in range(remaining_samples)])
             max_ctr = self.max_ctr(self.min_width)
             min_ctr = self.min_ctr(self.min_width)
@@ -202,7 +204,7 @@ class AbstractRandomTrainingDataGenerator(ABC):
                 temp_ctrs = (max_ctr - min_ctr) * np.random.rand(remaining_samples) + min_ctr
             else:
                 temp_ctrs = np.array([self._center_fcn() for _ in range(remaining_samples)])
-                                
+
             idx_list= []
             for num, (w,c) in enumerate(zip(temp_widths, temp_ctrs)):
                 if ((c >= self.min_ctr(w)) & (c <= self.max_ctr(w))):
@@ -210,25 +212,25 @@ class AbstractRandomTrainingDataGenerator(ABC):
             temp_ctrs = temp_ctrs[idx_list]
             temp_amps = temp_amps[idx_list]
             temp_widths = temp_widths[idx_list]
-            
+
             ctr_list.extend(temp_ctrs.tolist())
             width_list.extend(temp_widths.tolist())
             amp_list.extend(temp_amps.tolist())
-            
-            remaining_samples -= temp_ctrs.size
-            self.conditions_ = np.vstack((amp_list, ctr_list, width_list)).T     
 
-    def generate(self):      
+            remaining_samples -= temp_ctrs.size
+            self.conditions_ = np.vstack((amp_list, ctr_list, width_list)).T
+
+    def generate(self):
         """Generate f_ and Hf_ based on self.conditions"""
         f = []
         Hf = []
-        
+
         temp = self.fcn(self.n, self.conditions_)
-        
+
         f = temp.real
         Hf = temp.imag
         del temp
-        
+
         if self.stack_Hf_f:
             self.f_ = np.vstack((f, Hf))
             self.Hf_ = np.vstack((Hf, -f))
@@ -236,7 +238,7 @@ class AbstractRandomTrainingDataGenerator(ABC):
         else:
             self.f_ = f
             self.Hf_ = Hf
-        
+
     # @abstractstaticmethod
     # def fwhm(width):
     #     raise NotImplementedError
@@ -244,7 +246,7 @@ class AbstractRandomTrainingDataGenerator(ABC):
     @abstractclassmethod
     def fcn(cls, n, cmat):
         raise NotImplementedError
-        
+
     @classmethod
     def _check_cmat_shape(cls, cmat):
         if cmat.ndim == 1:
@@ -255,18 +257,18 @@ class AbstractRandomTrainingDataGenerator(ABC):
             return cmat
         else:
             raise ValueError('cmat need be a 2D array')
-        
+
     def __repr__(self):
         if self.f_ is None:
             return 'Empty'
         else:
             return '{} training samples generated ({} features)'.format(self.f_.shape[0], self.f_.shape[1])
-        
+
 class LorentzianTrainingData(AbstractRandomTrainingDataGenerator):
-    """ Generate Lorentzian-Dispersive synthetic data (see notes). Unless 
+    """Generate Lorentzian-Dispersive synthetic data (see notes). Unless
     stack_Hf_f = True, f_ will be the even function (Lorentzian), and Hf_ will
     be the odd function (Dispersive).
-    
+
     Parameters
     ----------
     n : array-like, shape (n_features,)
@@ -278,14 +280,14 @@ class LorentzianTrainingData(AbstractRandomTrainingDataGenerator):
     random_state : int, optional
         Random number generator seed, by default None
     amp : float, list, or function-like
-        Amplitude constant, list of [min,max) for uniform distribution, 
+        Amplitude constant, list of [min,max) for uniform distribution,
         or function to generate an amplitude.
-    
+
     Attributes
     ----------
     config : dict
         Configuration settings
-        
+
     Notes
     -----
     This class generates the Lorentzian - Dispersive lineshape pair, sometimes
@@ -293,11 +295,11 @@ class LorentzianTrainingData(AbstractRandomTrainingDataGenerator):
 
     .. math:: f(x) + iH\{f\}(x) = \frac{A\Gamma}{\Omega - n - i\Gamma}
 
-    where :math:`\Omega` is the center position, :math:`\Gamma` is the 
-    half-width, and :math:`A` is the amplitude. 
-    
-    -   **Note** this is not a standard definition (i.e., an extra 
-        :math:`\Gamma` in the numerator), but we have used it so that 
+    where :math:`\Omega` is the center position, :math:`\Gamma` is the
+    half-width, and :math:`A` is the amplitude.
+
+    -   **Note** this is not a standard definition (i.e., an extra
+        :math:`\Gamma` in the numerator), but we have used it so that
         the user can select the amplitude directly.
 
     .. [1] A. D. Poularikas, "Hilbert Transform," in The Handbook of Forumulas
@@ -313,24 +315,24 @@ class LorentzianTrainingData(AbstractRandomTrainingDataGenerator):
     @classmethod
     def fcn(cls, n, cmat):
         """ Returns a Lorentzian (real part) and its Hilbert transform (imag part) """
-        
+
         cmat = cls._check_cmat_shape(cmat)
         amp = cmat[...,0][:,None]
         ctr = cmat[...,1][:,None]
         width = cmat[...,2][:,None]
-        
+
         return amp*width*width/((n-ctr)**2 + width**2) + 1j*amp*width*(n-ctr) / ((n-ctr)**2 + width**2)
 
     @staticmethod
     def fwhm(width):
         """Full-width at half-max, where width is Gamma"""
         return 2*width
-    
+
 class GaussianTrainingData(AbstractRandomTrainingDataGenerator):
-    """ Generate Gaussian-Dawson synthetic data (see notes). Unless 
+    """ Generate Gaussian-Dawson synthetic data (see notes). Unless
     stack_Hf_f = True, f_ will be the even function (Gaussian), and Hf_ will
     be the odd function (Dawson).
-    
+
     Parameters
     ----------
     n : array-like, shape (n_features,)
@@ -342,26 +344,26 @@ class GaussianTrainingData(AbstractRandomTrainingDataGenerator):
     random_state : int, optional
         Random number generator seed, by default None
     amp : float, list, or function-like
-        Amplitude constant, list of [min,max) for uniform distribution, 
+        Amplitude constant, list of [min,max) for uniform distribution,
         or function to generate an amplitude.
-    
+
     Attributes
     ----------
     config : dict
         Configuration settings
-        
+
     Notes
     -----
     This class generates the Gaussian - Dawson lineshape pair [1]_.
 
-    .. math:: 
-    
+    .. math::
+
         f(x) + iH\{f\}(x) = A \exp \left [-\frac{(n - n_0)^2}{2\sigma^2} \right] + \\
         i\frac{2A}{\sqrt{\pi}} \mathcal{D}\{\sqrt{\frac{1}{2\sigma^2}}(n-n_0)\}
 
-    where :math:`n_0` is the center position, :math:`\sigma` is a width 
-    parameter, and :math:`A` is the amplitude. 
-    
+    where :math:`n_0` is the center position, :math:`\sigma` is a width
+    parameter, and :math:`A` is the amplitude.
+
     .. [1] https://en.wikipedia.org/wiki/Dawson_function#Relation_to_Hilbert_transform_of_Gaussian
 
 
@@ -374,24 +376,24 @@ class GaussianTrainingData(AbstractRandomTrainingDataGenerator):
     @classmethod
     def fcn(cls, n, cmat):
         """ Returns a Gaussian (real part) and its Hilbert transform (imag part) """
-        
+
         cmat = cls._check_cmat_shape(cmat)
         amp = cmat[:,0][:,None]
         ctr = cmat[:,1][:,None]
         width = cmat[:,2][:,None]
-        
+
         return amp * np.exp(-(n-ctr)**2/(2*width**2)) + 1j*(amp * 2 / np.sqrt(np.pi) * dawsn(np.sqrt(1/(2*width**2))*(n-ctr)))
 
     @staticmethod
     def fwhm(width):
         """Full-width at half-max, where width is sigma"""
         return 2*width*np.sqrt(2*np.log(2))
-    
+
 class SincTrainingData(AbstractRandomTrainingDataGenerator):
-    """ Generate Sinc and Hilbert of Sinc synthetic data pair (see notes). Unless 
+    """ Generate Sinc and Hilbert of Sinc synthetic data pair (see notes). Unless
     stack_Hf_f = True, f_ will be the even function (Sinc), and Hf_ will
     be the odd function (Hilbert of Sinc).
-    
+
     Parameters
     ----------
     n : array-like, shape (n_features,)
@@ -402,26 +404,26 @@ class SincTrainingData(AbstractRandomTrainingDataGenerator):
         Number of samples to generate, by default 1000
     random_state : int, optional
         Random number generator seed, by default None
-    
+
     Attributes
     ----------
     config : dict
         Configuration settings
-        
+
     Notes
     -----
     This class generates the Sinc - Hilbert of Sinc lineshape pair [1]_.
 
-    .. math:: 
-    
+    .. math::
+
         f(x) + iH\{f\}(x) = A \text{sinc}[2f(n-n_0)] + iA\frac{1 - \cos[2f(n-n_0)]}{2f\pi(n-n_0)}
 
-    where :math:`n_0` is the center position, :math:`f` is a frequency 
-    parameter inversely proportional to width, and :math:`A` is the amplitude. 
-    
-    .. [1] A. D. Poularikas, "Hilbert Transform," in The Handbook of Forumulas 
+    where :math:`n_0` is the center position, :math:`f` is a frequency
+    parameter inversely proportional to width, and :math:`A` is the amplitude.
+
+    .. [1] A. D. Poularikas, "Hilbert Transform," in The Handbook of Forumulas
         and Tables for Signal Processing, A. D. Poularikas, ed. (CRC, 1999).
-    
+
 
     """
     m_width_max_to_max = 2.459018059619502  # Empirical
@@ -429,22 +431,22 @@ class SincTrainingData(AbstractRandomTrainingDataGenerator):
               'width_over_Dn_is_max_width' : 1 / m_width_max_to_max,  # 1 FWHM is max width
               'width_over_dn_is_min_width':9 / m_width_max_to_max}  # min FWHM = 3 dn
 
-            
+
     @classmethod
     def fcn(cls, n, cmat):
         """ Returns a Sinc (real part) and its Hilbert transform (imag part) """
-        
+
         cmat = cls._check_cmat_shape(cmat)
         amp = cmat[:,0][:,None]
         ctr = cmat[:,1][:,None]
         width = cmat[:,2][:,None]
-        
+
         f = 1/width
         denom = 2*f*np.pi*(n-ctr)
         denom[denom == 0] = 1
 
         return amp*np.sinc(2*f*(n-ctr)) + 1j*amp*((1-np.cos(2*f*np.pi*(n-ctr)))/denom)
-        
+
 class SyntheticSpectra:
     """Generate synthetic spectra/signals
 
@@ -453,20 +455,20 @@ class SyntheticSpectra:
         n : array-like, shape (n_features,)
             The independent variable
         lineshape_inst : instance subclassed from AbstractRandomTrainingDataGenerator
-            Instance of a class that generates lineshapes and their Hilbert 
+            Instance of a class that generates lineshapes and their Hilbert
             transform, example:
-            LorentzianTrainingData(n, n_samples=10, amp=[0.1, 3.0], 
+            LorentzianTrainingData(n, n_samples=10, amp=[0.1, 3.0],
                                    center=None, width=[1.2, 15])
         n_spectra : int, optional
             Number of spectra/signal to generate, by default 100
         n_peak_lims : list of 2 ints, optional
-            [Min, Max) number of peaks that can be in a single spectrum, 
+            [Min, Max) number of peaks that can be in a single spectrum,
             by default [1,30]
-        """              
-    def __init__(self, lineshape_inst, n_spectra=100, n_peak_lims=[1,30], 
-                 f_is_even=True):    
-           
-        self._can_update = False 
+        """
+    def __init__(self, lineshape_inst, n_spectra=100, n_peak_lims=[1,30],
+                 f_is_even=True):
+
+        self._can_update = False
 
         self._lineshape_inst = lineshape_inst
         self._n_spectra = None
@@ -477,10 +479,10 @@ class SyntheticSpectra:
         self.n_peak_lims = n_peak_lims
         self.f_is_even = f_is_even
         self.lineshape_inst = lineshape_inst
-        self._can_update = True 
+        self._can_update = True
 
         self.regenerate()
-        
+
 
     def regenerate(self):
         """ Regenerate new set of spectra/signals"""
@@ -490,9 +492,9 @@ class SyntheticSpectra:
         self.Hf_ = []
         self.n_peaks_ = []
         self.conditions_ = []
-        
-        for num in range(self.n_spectra):
-            n_peaks = np.random.randint(self.n_peak_lims[0], 
+
+        for _ in range(self.n_spectra):
+            n_peaks = np.random.randint(self.n_peak_lims[0],
                                         self.n_peak_lims[1])
             self.n_peaks_.append(n_peaks)
             self.lineshape_inst.n_samples = n_peaks
@@ -546,12 +548,11 @@ class SyntheticSpectra:
 
 if __name__ == '__main__':
     n = np.linspace(-100,100,1001)
-    out = LorentzianTrainingData(n, stack_Hf_f=False, n_samples=1000, 
+    out = LorentzianTrainingData(n, stack_Hf_f=False, n_samples=1000,
                                  random_state=0, amp=lambda: 1, width=[1,20.])
 
-    
 
 
 
 
-    
+
