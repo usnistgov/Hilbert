199d0b4b0e2f759eaaeae9cc8c10631f85c03208
Charles H. Camp Jr <charles.camp@nist.gov>
2022-03-09 17:03:21 -0500

    Created compatible sklearn estimators

--- hilbert/__init__.py
+++ hilbert/__init__.py
@@ -1,5 +1,6 @@
-from .dft import hilbert_fft_henrici, hilbert_fft_marple
+from .dft import hilbert_fft_henrici, hilbert_fft_marple, hilbert_scipy
 from .wavelet import hilbert_haar
+from .estimators import DHT, DHT_Pad, LeDHT
 
 from .synthdata import GaussianTrainingData, LorentzianTrainingData, SincTrainingData, SyntheticSpectra
 from .preprocess import pad, depad, hilbert_pad_wrap, hilbert_pad_simple
@@ -10,8 +11,8 @@ from ._version import __version__
 # Can be set to whatever flavor of Hilbert-fft a user wants to use
 hilbert_fft = hilbert_fft_henrici
 
-__all__ = ['hilbert_fft', 'hilbert_fft_marple', 'hilbert_fft_henrici',
-           '__version__', 'MLHilb', 'GaussianTrainingData',
+__all__ = ['DHT', 'DHT_Pad', 'LeDHT', 'hilbert_fft', 'hilbert_fft_marple',
+           'hilbert_fft_henrici', 'hilbert_scipy', '__version__', 'MLHilb', 
            'LorentzianTrainingData', 'SincTrainingData',
            '__version__', 'pad', 'depad', 'hilbert_pad_wrap',
            'hilbert_pad_simple','rss','mse', 'mlhilb_scorer',
---
+++ hilbert/estimators.py
@@ -0,0 +1,77 @@
+"""
+Scikit-Learn compatible estimators for Hilbert transforms
+
+"""
+import numpy as np
+from sklearn.linear_model import LinearRegression
+from sklearn.base import RegressorMixin, BaseEstimator
+from scipy.signal import hilbert as _scipy_hilbert
+from hilbert.preprocess import hilbert_pad_simple
+from hilbert.dft import hilbert_scipy
+
+class DHT(BaseEstimator, RegressorMixin):
+    """ Class that performs the DHT. Default is SciPy's DHT """
+    def __init__(self, step_size=30000, 
+                 hilbert_fcn=hilbert_scipy):
+        super().__init__()
+        self.step_size = step_size
+        self.hilbert_fcn = hilbert_fcn
+        
+    def fit(self, X, y):
+        pass
+        
+    def predict(self, X):
+        assert (X.ndim <= 2), '3D not setup for this file'
+        if X.shape[0] > self.step_size:
+            out = 0*X
+            left_to_do = X.shape[0]
+            for num in range(X.shape[0] // self.step_size + X.shape[0] % self.step_size):
+                if left_to_do >= self.step_size:
+                    out[num*self.step_size:(num+1)*self.step_size,:] = self.hilbert_fcn(X[num*self.step_size:(num+1)*self.step_size,:])
+                    left_to_do -= self.step_size
+                else:
+                    out[num*self.step_size:,:] = self.hilbert_fcn(X[num*self.step_size:,:])
+                    left_to_do = 0
+            return out                    
+        else:
+            return self.hilbert_fcn(X)
+
+class DHT_Pad(BaseEstimator, RegressorMixin):
+    """ Class that pads the input data and performs the DHT from SciPy """
+    def __init__(self, hilbert_fcn=hilbert_scipy, 
+                 pad_factor=1, step_size=30000):
+        super().__init__()
+        self.pad_factor = pad_factor
+        self.step_size = step_size
+        self.hilbert_fcn = hilbert_fcn
+        
+    def fit(self, X, y):
+        pass
+        
+    def predict(self, X):
+        assert (X.ndim <= 2), '3D not setup for this file'
+
+        if X.shape[0] > self.step_size:
+            out = 0*X
+            left_to_do = X.shape[0]
+            for num in range(X.shape[0] // self.step_size + X.shape[0] % self.step_size):
+                if left_to_do >= self.step_size:
+                    out[num*self.step_size:(num+1)*self.step_size,:] = hilbert_pad_simple(X[num*self.step_size:(num+1)*self.step_size,:], self.hilbert_fcn, m_multi_size=self.pad_factor)
+                    left_to_do -= self.step_size
+                else:
+                    out[num*self.step_size:,:] = hilbert_pad_simple(X[num*self.step_size:,:], self.hilbert_fcn, m_multi_size=self.pad_factor)
+                    left_to_do = 0
+            return out                    
+        else:
+            return hilbert_pad_simple(X, self.hilbert_fcn, m_multi_size=self.pad_factor)
+
+
+class LeDHT(RegressorMixin, BaseEstimator):
+    def __init__(self, regressor_inst):
+        self.regressor_inst = regressor_inst
+    
+    def fit(self, X, y):
+        self.regressor_inst.fit(X, y)
+    
+    def predict(self, X):
+        return np.dot(X, self.regressor_inst.coef_.T) + self.regressor_inst.intercept_
