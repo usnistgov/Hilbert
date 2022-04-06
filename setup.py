35317fe57418dd6e4bc8ed91f6724adb9bc6fd24
Charles H. Camp Jr <charles.camp@nist.gov>
2022-04-06 16:45:35 -0400

    Continued moving to hilbert_toolkit

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ import os
 from setuptools import find_packages, setup
 
 # get __version__ from _version.py
-ver_file = os.path.join('hilbert', '_version.py')
+ver_file = os.path.join('hilbert_toolkit', '_version.py')
 with open(ver_file) as f:
     exec(f.read())
 
